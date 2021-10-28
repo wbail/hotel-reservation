c087dc08737662f422d14d3af833934b3bf8109b
wbail <guilhermedanbail@gmail.com>
2021-10-28 22:07:41 +0100

    refact: removed unnecessary imports

--- Hotel.Reservation.Application/Startup.cs
+++ Hotel.Reservation.Application/Startup.cs
@@ -1,16 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Hotel.Reservation.Application.Configurations;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using Hotel.Reservation.Application.Configurations;
 
 namespace Hotel.Reservation.Application
 {
